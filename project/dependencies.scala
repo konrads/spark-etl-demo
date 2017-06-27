81dddcaa5c1dea42df7306eb529251db7839ff52
Konrad Sosnowski <konrad.sosnowski@ubnt.com>
2017-06-28 08:09:59 +1000

    bringing back joda, used in default env vars

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -7,7 +7,7 @@ object dependencies {
     Seq(
     "org.apache.spark" %% "spark-core"   % sparkVsn % "provided",
     "org.apache.spark" %% "spark-sql"    % sparkVsn % "provided",
-    "net.jcazevedo"    %% "moultingyaml" % "0.4.0"    exclude("joda-time", "joda-time") exclude("org.joda", "joda-convert") exclude("com.github.nscala-time", "nscala-time_2.11"),
+    "net.jcazevedo"    %% "moultingyaml" % "0.4.0",               // exclude("joda-time", "joda-time") exclude("org.joda", "joda-convert") exclude("com.github.nscala-time", "nscala-time_2.11"),
     "org.rogach"       %% "scallop"      % "2.0.2"
   )
 }
